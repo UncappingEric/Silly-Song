e2019b84236c92793ffd76dff5aa25339d8923b8
Eric Cajuste <eric@erics-mbp.fios-router.home>
2017-09-24 20:12:00 -0400

    Initial Commit

---
+++ Silly Song/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Silly Song
+//
+//  Created by Eric Cajuste on 9/24/17.
+//  Copyright © 2017 Eric Cajuste. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
