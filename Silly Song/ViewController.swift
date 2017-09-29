1ffb9deffa12d2a4c5b02100e711a6a65b8bc79a
Eric Cajuste <eric_cajuste2000@yahoo.com>
2017-09-28 21:52:17 -0400

    completed project

--- Silly Song/ViewController.swift
+++ Silly Song/ViewController.swift
@@ -8,18 +8,58 @@
 
 import UIKit
 
+func shortNameFromName(name: String) -> String {
+    var name = name.lowercased()
+    var short = name.lowercased().unicodeScalars
+    let vowels = CharacterSet(charactersIn: "aeiou")
+    if name.rangeOfCharacter(from: vowels) != nil {
+        while !vowels.contains(short.removeFirst()) {
+            name.removeFirst()
+        }
+    }
+    return name
+}
+
+func lyricsForName(lyricsTemplate: String, fullName: String) -> String {
+    return lyricsTemplate.replacingOccurrences(of: "<FULL_NAME>", with: fullName)
+        .replacingOccurrences(of: "<SHORT_NAME>", with: shortNameFromName(name: fullName))
+}
+
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var lyricsView: UITextView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        nameField.delegate = self
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func reset(_ sender: Any) {
+        nameField.text = ""
+        lyricsView.text = ""
     }
-
-
+    
+    @IBAction func displayLyrics(_ sender: Any) {
+        if nameField.text?.characters.count != 0 {
+            let lyrics = lyricsForName(lyricsTemplate: bananaFanaTemplate, fullName: nameField.text!)
+            lyricsView.text = lyrics
+        }
+    }
+    
+    let bananaFanaTemplate = [
+        "<FULL_NAME>, <FULL_NAME>, Bo B<SHORT_NAME>",
+        "Banana Fana Fo F<SHORT_NAME>",
+        "Me My Mo M<SHORT_NAME>",
+        "<FULL_NAME>"].joined(separator: "\n")
+    
 }
 
+// MARK: - UITextFieldDelegate
+extension ViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return false
+    }
+}
